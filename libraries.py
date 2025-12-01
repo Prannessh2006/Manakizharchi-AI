227d02809c78897e1be4f00a33994f6b5c04e0e4
Prannessh KVA <99811495+Prannessh2006@users.noreply.github.com>
2025-12-02 00:32:30 +0530

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,22 @@
+import urllib3
+from instagrapi import Client
+import emoji
+import google.generativeai as genai
+from sentence_transformers import SentenceTransformer
+from instagrapi import Client
+import emoji
+import string
+import sentence_transformers
+from sentence_transformers import SentenceTransformer
+from chromadb.config import Settings
+import chromadb
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.sentiment import SentimentIntensityAnalyzer
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+import string
